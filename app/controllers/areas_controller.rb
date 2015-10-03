f92be7c6109e0ba8563687416cf36253e6590a78
Russell Huxhold <rhuxhold@Russells-MacBook-Air.local>
2015-10-03 15:37:59 -0500

    checkpoint 1 hw start

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -5,27 +5,35 @@ class AreasController < ApplicationController
   end
 
   def circle
-    pi = 3.1416 # Close enough.
-    @r = params[:radius]
+    @pi = 3.1416 # Close enough.
+    @r = params[:radius].to_f
+    @area = @pi * (@r**2)
   end
 
   def triangle
     @b = params[:base].to_f
     @h = params[:vertical_height].to_f
-    area = 0.5 * b * h
+    @area = 0.5 * @b * @h
   end
 
   def rectangle
-    @w = params[:width].to_f
-    @h = params[:height].to_f
+    @w = params[:horizontal_width].to_f
+    @h = params[:vertical_height].to_f
     @area = @w * @h
   end
 
+  def trap
+    @a = params[:length_of_side].to_f
+    @c = params[:length_of_side].to_f
+    @h = params[:vertical_height].to_f
+    @area = (1/2 * (@a + @c)) * @h
+  end
+
   def ellipse
-    pi = 3.1416 # Close enough.
-    @a = params[:a].to_f
-    @b = params[:b].to_f
-    @area = pi * @a * @b
+    @pi = 3.1416 # Close enough.
+    @w = params[:horizontal_width].to_f
+    @h = params[:vertical_height].to_f
+    @area = @pi * @w * @h
   end
 
   def random
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get("/areas/circle/:radius", { :controller => "areas", :action => "circle" })
 
-  get("/areas/triangle/base/vertical_height", { :controller => "areas", :action => "triangle" })
+  get("/areas/triangle/:base/:vertical_height", { :controller => "areas", :action => "triangle" })
 
   get("/areas/trapezoid/:a/:b/:vertical_height", { :controller => "areas", :action => "trapezoid" })
 
